7e5a9434cac58ce2ad66fb39baac8d63dbc44e3e
Artur Teben <teben.artur99@gmail.com>
2020-07-16 09:24:56 +0300

    Add description of class

--- src/com/cursor/java/homework/Main.java
+++ src/com/cursor/java/homework/Main.java
@@ -1,5 +1,7 @@
 package com.cursor.java.homework;
-
+//Author: Artur Teben
+//Date: June 20 2020
+//E-mail: teben.artur99@gmail.com
 public class Main{
 
     public static void main(String []args){
